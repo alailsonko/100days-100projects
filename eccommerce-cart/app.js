03b1121d89899550ef4203da39652807205d20f9
alailsonko <aalailson3@gmail.com>
2020-07-18 11:31:05 -0300

    setting the localstorage for products

--- eccommerce-cart/app.js
+++ eccommerce-cart/app.js
@@ -23,7 +23,7 @@ class Products{
       products = products.map((item)=>{
         const { title, price } = item.fields;
         const { id } = item.sys;
-        const {image} = item.fields.image.fields.file.url;
+        const image = item.fields.image.fields.file.url;
         return {title, price, id, image} 
       })
       return products
@@ -36,18 +36,41 @@ class Products{
 
 // display products
 class UI{
-
+    displayProducts(products){
+        let result = '';
+        products.forEach((product) => {
+          result +=`
+          <article class="product">
+          <div class="img-container">
+            <img src=${product.image} class="product-img" alt="product" />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart"></i>
+              add to bag
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>${product.price}</h4>
+        </article>
+          `
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 //local storage
 class Storage{
-
+     static saveProducts(products){
+       localStorage.setItem("products", JSON.stringify(products))
+     }
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
    const ui = new UI();
    const products = new Products();
-   
+   c
    //get all products
-   products.getProducts().then(data => console.log(data))
+   products.getProducts().then(products => { 
+     ui.displayProducts(products)
+     Storage.saveProducts(products)
+   })
 })
