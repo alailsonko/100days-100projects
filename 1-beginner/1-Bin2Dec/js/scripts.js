7292a444296a12f1698ed02c4778181a51b25773
alailsonko <aalailson3@gmail.com>
2020-10-14 01:56:52 -0300

    bin2dec: implements output of parsed bin to dec

--- 1-beginner/1-Bin2Dec/js/scripts.js
+++ 1-beginner/1-Bin2Dec/js/scripts.js
@@ -5,10 +5,28 @@ function loaded() {
   console.log("loaded")
 }
 
+function bin_to_dec(bstr) { 
+    return parseInt((bstr + '').replace(/[^01]/gi, ''), 2);
+}
 
-function onChangeInputBin() {  
+function onChangeInputBin() {
+
     var x = document.getElementById("input-bin").value
+    
     r = /^([0-1]{1,8})+$/
-    console.log(x.match(r)[1])
-    return x
+    
+    const value = x.match(r)
+
+    console.log(value)
+    
+    if(value === null){
+        alert("please insert a valid value")
+        return
+    }
+   if(value[0].length > 8){
+       alert("can not be more than 8")
+       return
+   }
+   document.getElementById("input-dec").innerHTML = bin_to_dec(value[1])
+    
 }
