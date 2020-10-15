27ff6b2e6adf1fcb6f053204f07048879278a997
alailsonko <aalailson3@gmail.com>
2020-10-15 13:07:36 -0300

    border-radius: customize ur border radius previewer

---
+++ 1-beginner/2-BorderRadiusPreviewer/js/index.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', loaded)
+document.getElementById("border-input").addEventListener('input', borderFunction)
+
+function loaded() {
+    console.log('javascript working!')
+}
+
+function borderFunction() {
+    let x = document.getElementById("border-input").value
+    console.log(x.slice(14,-1))
+//    document.getElementById("square-border-radius").style.borderRadius = "25px";
+   document.getElementById('square-border-radius').style.borderRadius = x.slice(14,-1);
+//    console.log(v)
+//    v = x.slice(14,);
+}
